463372ed770ee9455c6730478c223fa2b00f004f
Cadoni-hue <rafinhacadoni@gmail.com>
2026-09-02 10:16:46 -0300

    lab03: entrga RA6326179

--- AULA03/main.py
+++ AULA03/main.py
@@ -1,10 +1,32 @@
 from mod_rh import cadastrar_colaborador, exibir_colaboradores
-lista_colaboradores = []
-while True:
-    i = input("1-Cadastrar colaborador\n2-Exibir colaboradores\n3-Sair")
-    if i == "1":
-       def cadastrar_colaborador():
-    elif i == "2":
-        exibir_colaboradores()
-    else: 
-        break
+def exibir_menu():
+    print("=== Sistema de Cadastro de Colaboradores ===")
+    print("1. Cadastrar colaborador")
+    print("2. Exibir colaboradores cadastrados")
+    print("3. Sair")
+
+def main():
+    colaboradores = []
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            nome = input("Digite o nome do colaborador: ")
+            cargo = input("Digite o cargo do colaborador: ")
+            while True:
+                salario = float(input("Digite o salário do colaborador: "))
+                if salario < 0:
+                    print("Salário inválido. Digite um valor positivo.")
+                else:
+                    break
+            colaborador = cadastrar_colaborador(nome, cargo, salario)
+            colaboradores.append(colaborador)
+            print("Colaborador cadastrado com sucesso!")
+        elif opcao == "2":
+                exibir_colaboradores(colaboradores)
+        elif opcao == "3":
+            print("Saindo do sistema...")
+            break
+        else:
+             print("Opção inválida. Escolha uma opção: ")
+main()
--- AULA03/mod_rh.py
+++ AULA03/mod_rh.py
@@ -1,5 +1,12 @@
-def cadastrar_colaborador(nome: str, cargo: str, salario: float) -> dict:
-    return {"nome": nome, "cargo": cargo, "salario": salario}
-def exibir_colaboradores(lista_colaboradores: list) -> None:
+def cadastrar_colaborador(nome: str, cargo: str, salario: float)->dict:
+    return{
+        "nome": nome,
+        "cargo": cargo,
+        "salario": salario
+    }
+def exibir_colaboradores (lista_colaboradores: list) -> None:
+    if not lista_colaboradores:
+        print("Nenhum colaborador cadastrado.\n")
+        return
     for colaborador in lista_colaboradores:
         print(f"Nome: {colaborador['nome']}, Cargo: {colaborador['cargo']}, Salário: {colaborador['salario']}")
